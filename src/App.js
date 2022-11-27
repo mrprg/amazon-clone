c5136256b940117662b45442b3aaf2465e3377d3
mrprg <mohamadrezaasadi322@gmail.com>
2022-11-28 00:30:57 +0330

    add header and home components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className='app'>App</div>
+    <div className='app'>
+      <Header/>
+      <Home/>
+    </div>
   )
 }
 
