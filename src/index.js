796b0c33b1df50a07a41cd193958e24fc4fd78db
mrprg <mohamadrezaasadi322@gmail.com>
2022-11-28 22:25:22 +0330

    making reactcontext file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './context/Stateprovider';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
+      <StateProvider initialState={iniinitialState} reducer={reducer} >
         <App />  
+      </StateProvider>
   </React.StrictMode>
 );
 
