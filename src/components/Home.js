84329613a4bad4e5259b3779082bfde6ba4e5d21
mrprg <mohamadrezaasadi322@gmail.com>
2022-11-28 16:25:29 +0330

    Making The Home Component And Style It

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home__container'>
+        <img className='home__image' src='https://images-na.ssl-images-amazon.com/images/G/15/digital/video/merch/Other/BRND_MTH21_SADLPDesktop_1453x363_Final_nolocale_PVD7436_Canada.jpg' alt='amazone' />
+        <div className='home__row'>
+          first row
+        </div>
+        <div className='home__row'>
+          second row
+        </div>
+        <div className='home__row'>
+            third row          
+        </div>
+      </div>
+    </div>
   )
 }
 
