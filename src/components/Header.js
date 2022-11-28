bca60d363bbb7ec33b47cbb9f54fa5476f029627
mrprg <mohamadrezaasadi322@gmail.com>
2022-11-28 16:09:21 +0330

    make the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Header.css'
+
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <img
+          className='header__logo' 
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt='logo'/>
+
+          <div className='header__search'>
+            <input className='header__searchInput' type="text" />
+            <SearchIcon className='header__searchIcon' />
+          </div>
+
+          <div className='header__nav'>
+
+            <div className='header__option'>
+                <span className='header__optionLineOne'>Hello guest</span>
+                <span className='header__optionLineTwo'>Sign in</span>
+            </div>
+
+            <div className='header__option'>
+                <span className='header__optionLineOne'>Returns</span>
+                <span className='header__optionLineTwo'>& Orders</span>
+            </div>
+
+            <div className='header__option'>
+                <span className='header__optionLineOne'>Your</span>
+                <span className='header__optionLineTwo'>Prime</span>
+            </div>
+
+            
+                
+            <div className='header__optionBasket'>
+                <ShoppingBasketIcon />
+                <span className='header__optionLineTwo header__basketCount'>0</span>
+            </div>
+                
+            
+                
+         </div>
+    </div>
   )
 }
 
