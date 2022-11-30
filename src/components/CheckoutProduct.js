44b23da289c253999a170bf1cee3f5674e9876fc
mrprg <mohamadrezaasadi322@gmail.com>
2022-11-30 20:10:03 +0330

    checkout product component

---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/checkoutproduct.css';
+
+function CheckoutProduct() {
+  return (
+    <div className='checkoutproduct'>
+        CheckoutProduct
+    </div>
+  )
+};
+
+export default CheckoutProduct;
