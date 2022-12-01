b0f87d57ffb0101d0e9623e79f4d0677c5a817d8
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-01 22:07:18 +0330

    add firebase file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCH8F7AMuNG57dGXlZLU6P1UjTCk8ZZ06c",
+    authDomain: "clone-d8694.firebaseapp.com",
+    projectId: "clone-d8694",
+    storageBucket: "clone-d8694.appspot.com",
+    messagingSenderId: "686785649052",
+    appId: "1:686785649052:web:607d3a4bb9bf5541409059",
+    measurementId: "G-J3JDGCTKR3"
+};
