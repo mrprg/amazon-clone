db44497e3a749ab319d979564204dd8e924131e5
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-01 18:00:07 +0330

    make login form in Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,6 +13,24 @@ function Login() {
         />
       </Link>
 
+      <div className='login__container'>
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type='text' />
+
+          <h5>Password</h5>
+          <input type='password' />
+
+          <button>Sign in</button>
+        </form>
+        <p>
+          By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our interest-based Ads Notice.
+        </p>
+
+        <button className='login__registerButton'>Create your Amazon Acount</button>
+      </div>
+
     </div>
   )
 }
