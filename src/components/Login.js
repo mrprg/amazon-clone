535673b05ddff1cf502593a04344948ce89d3fbe
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-01 17:43:32 +0330

    add login_logo to the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Login.css';
 
 function Login() {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <Link to='/'>
+        <img 
+        className='login__logo'
+        src='https://api.freelogodesign.org/assets/blog/img/20180911090509731amazon_logo_RGB.jpg'
+        alt='logo'
+        />
+      </Link>
+
+    </div>
   )
 }
 
