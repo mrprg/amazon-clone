44b7f0236f11d68a139fdd6099526e59e365babb
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-03 16:49:06 +0330

    add evevntListener (useEffect)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
 import Login from './components/Login';
 
+
 function App() {
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <Router>  
       <div className="App">
