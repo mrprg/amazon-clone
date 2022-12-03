b60d0f7006641ece18e389243205e6bd0d43b050
mrprg <mohamadrezaasadi322@gmail.com>
2022-12-03 16:27:12 +0330

    add useNavigate to the Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { auth } from '../firebase/firebase';
 import '../styles/Login.css';
 
 function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   
   const signIn = e => {
     e.preventDefault();
+
+    auth
+        .signInWithEmailAndPassword(email, password)
+        .then((auth) => {
+              if (auth) {
+                navigate('/')
+              }
+        })
+        .catch(error => {
+          alert(error.message)
+        })
   }
 
   const register = (e) => {
@@ -18,6 +30,9 @@ function Login() {
         .createUserWithEmailAndPassword(email,password)
         .then((auth) => {
           console.log(auth)
+          if (auth) {
+            navigate('/')
+          }
         })
         .catch(error => alert(error.message)) 
 
